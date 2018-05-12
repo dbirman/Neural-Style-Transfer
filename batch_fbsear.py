f4df46198874a61635105c68a284ef8348e12448
Dan Birman <danbirman@gmail.com>
2018-05-12 16:19:08 -0700

    Creating new batch function to push to sherlock GPU

--- batch_fbsear.py
+++ batch_fbsear.py
@@ -1,33 +1,48 @@
 import os
 import subprocess
 
-ipath = "images/fbsear/"
-opath  = "images/fbsear/out"
+ipath = "images/fbsear/objects"
+opath  = "images/fbsear/objects/out"
 
-cats = ["banana","cat","doge","leek"]
+d = ipath
+dirs = [o for o in os.listdir(d) if ("out" not in o) and os.path.isdir(os.path.join(d,o))]
 
-for content in cats:
-	for style in cats:
+front_call = "sbatch -p hns --gres gpu:1 --mem=5000 --time=00:30:00 --wrap=\"module load py-tensorflow; module load py-scipystack; cd $HOME/Neural-Style-Transfer; "
+
+dirs = dirs[0:2]
+print(dirs)
+
+for content in dirs:
+	for style in dirs:
 		if style != content: 
-			for ci in range(1,5):
-				for si in range(1,5):
+			for ci in [1]:
+				for si in [1]:
+			# for ci in range(1,5):
+				# for si in range(1,5):
 					content_image = os.path.join(ipath,content,str(ci)+".jpg")
 					style_image = os.path.join(ipath,style,str(si)+".jpg")
 
 					# check folder
-					ofolder = os.path.join(opath,content+"_"+style)
+					ofolder = os.path.join(opath,style+content)
 					if not os.path.exists(ofolder):
 						os.makedirs(ofolder)
 
-					opfx = os.path.join(ofolder,str(ci)+"_"+str(si))
+					opfx = os.path.join(ofolder,str(si)+str(ci))
 
 					# create call
 					call = "python ~/proj/Neural-Style-Transfer/inetwork.py " \
-						"\"./" + content_image + "\" " \
-						"\"./" + style_image + "\" " \
-						"\"./" + opfx + "\" " \
-						""
+						"\'./" + content_image + "\' " \
+						"\'./" + style_image + "\' " \
+						"\'./" + opfx + "\' " \
+						"\""
 
-					print(call)
-					subprocess.call(call,shell=True)
+					totalcall = front_call+call
+
+					print('*********************************')
+					print('* creating call for: ************')
+					print('*********************************')
+					print(totalcall)
+					print('*********************************')
+					# subprocess.call(call,shell=True)
 					# os.system(call)
+					print('*********************************')
